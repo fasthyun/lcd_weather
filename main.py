54366848d82583450509d816a8a85fc1c1cb6bb2
hyun <daehyun.yang@gmail.com>
2024-01-10 20:03:25 +0900

    update

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 
+from PyQt5.QtCore import QTimer
+
+
+def timeout():
+    pass
+
+
+timer = QTimer()
+timer.start(1000)
+timer.timeout.connect(timeout)
 
 app = QApplication([])
 window = QWidget()
@@ -9,5 +19,5 @@ layout.addWidget(QPushButton('Bottom'))
 window.setLayout(layout)
 window.show()
 
-
 app.exec()
+del app 
