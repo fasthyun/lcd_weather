a9aef931e1c4d93b769b06e15060ed206f17048f
hyun.dell3630 <hyun@dell3630>
2024-02-07 21:34:23 +0900

    d

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
+# work for pyhton3.6 
+# because of 
+#    1. ordreddict 
+#    2. 
+
+""" 
+design 철학  
+===========
+    화면표시부와 네트워크 데이타 처리부를 따로 분리하고 
+    그렇게 분리하는게 낫겠다 싶었는데   잘 안되었다.
+
+ * qlayout이나 qlabel을 사용하지 않은 이유는 여러가지 제한 사항이 발생해서 그렇다.
+       예를 들어 qt stylesheet는 기능은 많지만 내가 필요로하는 속성의 제한, 직접  애니메이션  넣기 제한 등등 
+  *  표시부 widget는 용도에따라 코드재사용을 할수있게 설계하였다.
+   
+ * Network처리부:
+      스트림데이타가 조각나서 오는경우에 대해 손실없이 처리하는것에 대해 몇년동안 고민해 왔는데 
+      이번 코드는  0x02, 0x03 시작 끝 Sync가 존재하는데 불구하고 이해하기 쉬우면서 꽤나 세련되게 
+      작성되었다. 앞으로도 네트워크프로그램 작성할때 계속 사용할것 같다. 진작..이런 방법으로 했으면 ㅋ 
+ 
+"""
+
 from PyQt5.QtWidgets import QApplication,QMainWindow, QWidget, QPushButton, QVBoxLayout
 from PyQt5.QtWidgets import QFrame
 from PyQt5.QtGui import QPainter, QImage, QPen, QColor
 from PyQt5.QtCore import QTimer,QRect , Qt
 from PyQt5 import uic
+
 #from PyQt5 import rc
 import weather_rc
+import time
+import datetime
 
-from socket_server import server
-
+from socket_server import Server,parse_KWEATHER
 
 form_ui, widget_class = uic.loadUiType('lcd_weather.ui')
 
+server=None
 
 #화면을 띄우는데 사용되는 Class 선언
 class MainWindow(widget_class,form_ui) :
@@ -18,27 +43,35 @@ class MainWindow(widget_class,form_ui) :
         super().__init__()
         self.setupUi(self)
 
-class AWidget(QWidget) :
-    def __init__(self, parent) :
-        super().__init__( parent)
-        print("parent= ", parent)
-        self.title1="awidget"
+class ItemWidget(QWidget) :
+    def __init__(self, parent,_title) :
+        super().__init__(parent)
+        #print("parent= ", parent)
+        self.title1=_title
         self.basecolor={"title":QColor(0,100,200) ,"value": None}
         self.config={"title":"title","pos":(0,0)}
-        self.move(0,0)        
+        self.move(0,0)      
+        self.value="0" # string is better???
     
+    def setValue(self,val):
+        self.value=val
+        #draw 
+        
     def changeSize(self,d):
-        gw=self.parentWidget().width()
-        gh=self.parentWidget().height()
+        pw=self.parentWidget().width()
+        ph=self.parentWidget().height()  - self.parentWidget().height()/10        
+        
         #print("w=",w," , h=",h)
         #self.setBaseSize(w/3,h/3)
-        self.resize(gw/3 -d ,gh/3 -d )    
         _height=self.height()        
         _width=self.width()
-        _d=_height/10
-        _f=self.font()
-        _f.setPixelSize(_height/3 - _d)
-        self.setFont(_f)
+        _wd=pw/100
+        _hd=ph/50
+        
+        self.resize(pw/3 -_wd*2 ,ph/3 -_hd*2 )  
+        #_f=self.font()
+        #_f.setPixelSize(_height/3 - _)
+        #self.setFont(_f)
         
     def showEvent(self,event):
         print("show parent = " ,self.parentWidget())
@@ -46,10 +79,31 @@ class AWidget(QWidget) :
         #h=self.parentWidget().height()
         #self.setBaseSize(w/3,h/3)
         pass
+    
+    def drawTitle(self, p ): # painter
+        _height=self.height()        
+        _width=self.width()
+        
+        font = p.font();
+        font.setPixelSize(_height/6);
+        
+        p.setFont(font);
+        
+        d=_height/10        
+        p.setBrush(self.basecolor['title'])
+        p.setPen(QPen(QColor(10, 10, 10), 1))        
+        p.drawRect(0,0,_width,_height/4 )
+        r=QRect(0,0,_width,_height/4)
+        #br=QRect(2,2,_width-2,_height/4 - )
+        p.setPen(QPen(QColor(255, 255, 255), 1))
+        p.drawText(r,Qt.AlignVCenter | Qt.AlignLeft,self.title1)
+        
     def paintEvent(self, event):
         #self.changeSize()
         _height=self.height()        
         _width=self.width()
+        
+        d=_height/40
         
         #print("w=",w," , h=",h)
         p = QPainter()
@@ -59,26 +113,59 @@ class AWidget(QWidget) :
         
         p.setBrush(QColor(255, 255, 255))
         p.setPen(QPen(QColor(60, 60, 60), 3))
-        p.drawRect(self.rect())
+        p.drawRect(self.rect())    
         
-        p.setPen(QPen(QColor(10, 10, 10), 1))
-                
-        p.setBrush(self.basecolor['title'])
-        p.setPen(QPen(QColor(10, 10, 10), 1))
+        self.drawTitle(p)
         
-        r=QRect(0,0,_width,_height/4)
-        p.drawRect(0,0,_width,_height/4)
-        
-        p.setPen(QPen(QColor(255, 255, 255), 1))
-        p.drawText(r,0,self.title1)
+        font = p.font();
+        font.setPixelSize(_height/3);         
+        p.setFont(font);
         
         y1=_height*1/4
         #y1=_height*1/4
         r=QRect(0,y1,_width,_height*3/4) 
-        p.setPen(QPen(QColor(1, 1, 1), 1))
-        p.drawText(r, Qt.AlignVCenter | Qt.AlignHCenter ,self.title1)
+        p.setPen(QPen(QColor(1, 1, 1), 10))
+        p.drawText(r, Qt.AlignVCenter | Qt.AlignHCenter ,str(self.value))        
+        
         p.end()
          
+class WindDirectionWidget(ItemWidget):
+    def __init__(self, parent,_title) :
+        super().__init__(parent,_title)
+        pass
+    
+    def setValue(self,_val):        
+        val=_val
+        if 350 < _val < 360 and  0< _val <20 : 
+            val="북"
+        if 20< _val <70 : 
+            val="북동"
+        if 70< _val <110 : 
+            val="동"        
+        if 110< _val <160 : 
+            val="남동"
+        if 160< _val <200 : 
+            val="남"
+        if 200< _val <250 : 
+            val="남서"            
+        if 250< _val <290 : 
+            val="서"
+        if 290< val <350 : 
+            val="북서"      
+        self.value=val
+        #dra
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        """
+        _height=self.height()        
+        _width=self.width()        
+        d=_height/40        
+        #print("w=",w," , h=",h)
+        p = QPainter()
+        p.begin(self)               
+        p.end()
+        """
+        pass
     
 class MyWindow(QMainWindow):         
     def __init__(self):
@@ -89,9 +176,16 @@ class MyWindow(QMainWindow):
         self.image_bg=QImage(":/image/sky_blue2.jpeg"); 
         
         self.widgets=[]
-        
-        for i in range(0,9):
-            self.widgets.append(AWidget(self))
+        #self.showFullScreen()
+        #self.setWindowState(self.windowState() ^ Qt.WindowFullScreen);
+        #self.setWindowState(self.windowState() ^ Qt.WindowFullScreen);
+        #for i in range(0,9):
+        for k in items.keys():
+            _it=items[k]
+            #_obj=ItemWidget(self,_it['title'])
+            _obj=_it['class'](self,_it['title'])
+            _it["object"]=_obj
+            self.widgets.append(_obj)
             
         #self.
     def resizeEvent(self, e):
@@ -116,34 +210,64 @@ class MyWindow(QMainWindow):
         
     def paintEvent(self, event):
         qp = QPainter()
-        qp.begin(self)
-      
+        qp.begin(self)      
         qp.drawImage(self.rect(),self.image_bg,self.image_bg.rect())
         qp.end()
         
+def onAppQuit():
+    if server is not None:
+        server.server_close()
+        print("close server!!")
+    #if app is not None:
+    #    app.closeAllWindows()
+    
+    
+#orderedDictionary
+items = {  
+   "wind_direction" : { "name":"", "title" :"풍향", "object": None ,"class":WindDirectionWidget },
+   "wind_speed":{ "name":"", "title" :"풍속", "object": None ,"class": ItemWidget },
+   "humidity": { "name":"", "title" :"습도", "object":  None ,"class":ItemWidget },
+   "temp_now": { "name":"", "title" :"현재기온", "object":  None ,"class": ItemWidget ,"style": ""}, # green
+   "temp_high_day":{ "name":"", "title" :"일최고기온", "object": None ,"class": ItemWidget },  # red 
+   "temp_low_day": { "name":"", "title" :"일최저기온", "object":  None ,"class": ItemWidget },  # cyan 
+   "rainfall": { "name":"", "title" :"강수량", "object":  None ,"class": ItemWidget },
+   "rainfall_yesterday": { "name":"humidity", "title" :"전일강수량", "object": None ,"class": ItemWidget },
+   "snowfall": { "name":"", "title" :"강설량", "object": None ,"class": ItemWidget }
+ }
+
+server=Server()
 app = QApplication([])
 #widget = uic.loadUi('lcd_weather.ui')
+app.aboutToQuit.connect(onAppQuit)
 
 def timeout():
-    server.timeout=0.01
-    server.handle_request()
-    print("timeout!")
+    print("DEBUG: timeout()!",datetime.datetime.now())
+    if server is not None:
+        server.handle_request() # process the quequed network data 
+        if server.que.qsize() !=0 :
+            _frame=server.que.get() 
+            _dict=parse_KWEATHER(_frame)
+            #print("received data _len= ",len(_data),_dict)
+            for k in items.keys():
+                if k in _dict:
+                    _val=_dict[k]
+                    _it =items[k]
+                    _it['object'].setValue(_val)
+    
+    #time.sleep(0.1)
     pass
 
 timer = QTimer()
-timer.start(1000)
+timer.start(4000)
 timer.timeout.connect(timeout)
+
 
 #window = widget #MainWindow()
 window = MyWindow()
-"""window = QWidget()
-layout = QVBoxLayout()
-layout.addWidget(QPushButton('Top'))
-layout.addWidget(QPushButton('Bottom'))
-window.setLayout(layout)
-"""
 window.show()
+
 app.exec()
+
+
 del app 
 del server
-
--- socket_server.py
+++ socket_server.py
@@ -1,8 +1,24 @@
 # for python3
-
+# test sample 
+_b1=b'\x020001,01MN,20231223170800,-3.8,-3.7,-3.8,52.1,52.7,51.7,1024.8,1024.8,1024.8,0.0,0.0,0.2,0.0,0.0,92.9,84.6,15.0\x03'
+_b2=b'\x02-0.5,-15.6,0.0\x03\r\n'
+"""
+b'\x020001,01MN,20231223170800,-3.8,-3.7,-3.8,52.1,52.7,51.7,1024.8,1024.8,1024.8,0.0,0.0,0.2,0.0,0.0,92.9,84.6,15.0\x03'
+b'\x02-0.5,-15.6,0.0\x03\r\n'
+b'\x020001,01MN,20231223170900,-3.8,-3.8,-3.8,53.6,53.9,53.3,1024.8,1024.8,1024.8,0.0,0.0,0.2,0.0,0.0,76.7,70.5,15.0\x03'
+b'\x02-0.5,-15.6,0.0\x03\r\n'
+b'\x020001,01MN,20231223171000,-3.9,-3.8,-3.9,53.6,54.0,53.3,1024.8,1024.9,1024.8,0.0,0.0,0.2,0.0,0.0,90.6,98.7,15.0\x03'
+b'\x02-0.5,-15.6,0.0\x03\r\n'
+b'\x020001,01MN,20231223171200,-4.0,-3.9,-4.0,52.4,52.5,52.4,1024.9,1024.9,1024.9,0.0,0.0,0.2,0.0,0.0,105.3,107.2,15.0\x03'
+b'\x02-0.5,-15.6,0.0\x03\r\n'
+b'\x020001,01MN,20231223171300,-4.0,-4.0,-4.0,52.3,52.4,52.3,1024.9,1025.0,1024.9,0.0,0.0,0.2,0.0,0.0,108.6,135.4,15.0\x03'
+b'\x02-0.5,-15.6,0.0\x03\r\n'
+"""
 
 from socketserver import TCPServer,BaseRequestHandler
+from datetime import datetime
 
+from queue import Queue
 """ 
  전부 문자열 ㄸㄸㄸㄸ
 ['0001', # 0  데이터로거 ID
@@ -26,63 +42,118 @@ from socketserver import TCPServer,BaseRequestHandler
  '84.6',  #18 풍향 360
  '15.0'] #19 로거 전압 
 """
-def parse_KWEATHER(_bytes):
-    if _bytes[0] == 0x02 and _bytes[-1] == 0x03:
-        print("ok")
-        _list=_bytes[1:-1].decode().split(',')
-        if len(_list) == 20:
-            _id=_list[0]
-            _type=_list[1]
-            _datetime=_list[2]
-            _temp1 = _list[3]
-            _temp2 = _list[4]
-            _temp3 = _list[5]
-            _hum1 = _list[6]
-            _hum2 = _list[7]
-            _hum3 = _list[8]
-            _press1 = _list[9]
-            _press2 = _list[10]
-            _press3 = _list[11]
-            _voltage= _list[19]
-        return _list
-    return ""
-    
 
-class HandleClass(BaseRequestHandler):
-    #def __init__(
+def parse_KWEATHER(_bytes):    
+    if _bytes[0] == 0x02 :
+        if _bytes[-1] == 0x03:            
+            _list=_bytes[1:-1].decode().split(',')
+            if len(_list) == 20:
+                print("DEBUG: 20 field found", _list[1])
+                _dict={
+                '_id' :_list[0],
+                '_type' :_list[1],
+                '_datetime':datetime.strptime(_list[2],"%Y%m%d%H%M%S"),
+                'temp_now' : _list[3],
+                'temp2' : _list[4],
+                'temp3' : _list[5],
+                'humidity':_list[6],
+                'hum2' : _list[7],
+                'hum3' : _list[8],
+                'press1' :_list[9],
+                'press2' : _list[10],
+                'press3' : _list[11],
+                '_none1' : _list[12],
+                '_none2' : _list[13],
+                'wind_speed_x' : _list[14],
+                'wind_speed' : _list[15],
+                '_none4' : _list[16],
+                'wind_direction_x' : _list[17],
+                'wind_direction' :_list[18],
+                '_voltage': _list[19]}
+                return _dict
+            elif len(_list) == 11 :
+                print("DEBUG: 11 field found", _list[1])
+                _dict={
+                '_id' :_list[0],
+                '_type' :_list[1],
+                '_datetime':datetime.strptime(_list[2],"%Y%m%d%H%M%S"),
+                'temp_now' : _list[3],
+                'humidity':_list[4],
+                'pressure' :_list[5],
+                'unknown1' : _list[6],
+                'unknown2' : _list[7],   
+                'wind_speed' : _list[8],  
+                'wind_direction' :_list[9],
+                '_voltage': _list[10]}
+                return _dict        
+            if len(_list) == 3:
+                _dict={}
+                print("DEBUG: 3 field found ",_bytes) 
+                _dict={
+                'temp_high_day' :_list[0],
+                'temp_low_day' :_list[1],               
+                'rainfall_yesterday' : _list[2]}
+                return _dict
+            else:
+                print("DEBUG: unknown length frame =",len(_list),_bytes)
+        
+    print("unknown data =",_bytes)
+    return {}
+
+class HandleClass(BaseRequestHandler):    
     def setup(self):
         pass
     def handle(self):
         sock=self.request 
-        data=sock.recv(1024)
-        print("happend?", self.client_address,type(self.request))
-        #print(" ===>", data)
+        _data=sock.recv(1024)
+        self.put(_data)    
+        print("DEBUG: transfered from : ", self.client_address,type(self.request),len(_data))        
+        while True:
+            _frame = self.getFrame()
+            if _frame is not None:
+                self.server.que.put(_frame)
+            else:
+                break 
         
     def finish(self):
         print("finished!")
 
-
-#server= TCPServer(("192.168.1.5",2000),HandleClass)
-server= TCPServer(("0.0.0.0",2000),HandleClass)
-server.timeout=0.1
+class Server(TCPServer):
+    def __init__(self):
+        TCPServer.__init__(self,("0.0.0.0",2000),HandleClass)
+        self.timeout=0.1
+        self.que=Queue() # temporaly!!!
+        self.buffer=bytes(0)
+    
+    def put(self,new_bytes):
+        #self.buffer = self.buffer + new_bytes
+        self.buffer += new_bytes
+        
+    def getFrame(self):
+        _bytes=self.buffer        
+        s=_bytes.find(0x02);
+        if s == -1 :            
+            return None 
+        e=_bytes.find(0x03);
+        if e == -1 :             
+            return None        
+        if s < e:
+            self.buffer= _bytes[e:]
+            return _bytes[s:e] # safer        
+        # something wrong !  end < start  so , 
+        self.buffer = _bytes[s:] # delete until start
+        return None 
+                
+        
 #server.serve_forever(0.1)
-server.handle_request()
+#server.handle_request()
+#server.server_close()
 
 
-_b1=b'\x020001,01MN,20231223170800,-3.8,-3.7,-3.8,52.1,52.7,51.7,1024.8,1024.8,1024.8,0.0,0.0,0.2,0.0,0.0,92.9,84.6,15.0\x03'
-_b2=b'\x02-0.5,-15.6,0.0\x03\r\n'
-
-c=parse_KWEATHER(_b1)
-"""
-b'\x020001,01MN,20231223170800,-3.8,-3.7,-3.8,52.1,52.7,51.7,1024.8,1024.8,1024.8,0.0,0.0,0.2,0.0,0.0,92.9,84.6,15.0\x03'
-b'\x02-0.5,-15.6,0.0\x03\r\n'
-b'\x020001,01MN,20231223170900,-3.8,-3.8,-3.8,53.6,53.9,53.3,1024.8,1024.8,1024.8,0.0,0.0,0.2,0.0,0.0,76.7,70.5,15.0\x03'
-b'\x02-0.5,-15.6,0.0\x03\r\n'
-b'\x020001,01MN,20231223171000,-3.9,-3.8,-3.9,53.6,54.0,53.3,1024.8,1024.9,1024.8,0.0,0.0,0.2,0.0,0.0,90.6,98.7,15.0\x03'
-b'\x02-0.5,-15.6,0.0\x03\r\n'
-b'\x020001,01MN,20231223171200,-4.0,-3.9,-4.0,52.4,52.5,52.4,1024.9,1024.9,1024.9,0.0,0.0,0.2,0.0,0.0,105.3,107.2,15.0\x03'
-b'\x02-0.5,-15.6,0.0\x03\r\n'
-b'\x020001,01MN,20231223171300,-4.0,-4.0,-4.0,52.3,52.4,52.3,1024.9,1025.0,1024.9,0.0,0.0,0.2,0.0,0.0,108.6,135.4,15.0\x03'
-b'\x02-0.5,-15.6,0.0\x03\r\n'
-
-"""
+if __name__ =="__main__" :    
+    c=parse_KWEATHER(_b1)
+    print(c)
+    c=parse_KWEATHER(_b2)
+    print(c)
+    pass
+print("__pacakge__",__package__)
